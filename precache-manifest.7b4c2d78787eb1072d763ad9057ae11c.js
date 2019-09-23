93a281b8c725fffb3f410e989aacf0d2ffa000e1
Arūnas <arunasjoskaudas@gmail.com>
2019-09-23 18:10:33 +0300

    Updates

---
+++ precache-manifest.7b4c2d78787eb1072d763ad9057ae11c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "096069497e36cbd683a7031722bc1327",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "bd5e6a1b7facf01c9324",
+    "url": "/ReactJS/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "15b2afa013856f4bc8b6",
+    "url": "/ReactJS/static/js/2.a923f730.chunk.js"
+  },
+  {
+    "revision": "bd5e6a1b7facf01c9324",
+    "url": "/ReactJS/static/js/main.f0a40f1b.chunk.js"
+  },
+  {
+    "revision": "dba24ef006963dfd35c2",
+    "url": "/ReactJS/static/js/runtime-main.170759ca.js"
+  }
+]);
